671458b8ed80a4da6c35966da8eabc54aa782394
okapi <jf@geonef.fr>
2014-09-21 14:07:52 +0200

    initial commit with sysconf/actual/ and READMEs

---
+++ actual/install.sh
@@ -0,0 +1,8 @@
+# Sysconf profile gitted.geoserver/actual -- Installer script  -*- shell-script -*-
+
+. /usr/lib/sysconf.base/common.sh
+
+# Install required Debian packages
+# _packages=
+# _packages="$_packages ..."
+# sysconf_require_packages $_packages
